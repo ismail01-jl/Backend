4b49be610766677f6e2d64315602021aa15c399c
ismail01-jl <jellitiismail2002@gmail.com>
2026-04-27 23:03:07 +0100

    Importing ML model & testing the API

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+import numpy as np
+import joblib
+from pydantic import BaseModel
+
 
 app = FastAPI()
+
+model = joblib.load("models/EcoSmart_Model.pkl")
 
 @app.get("/")
 def read_root():
     return {"message": "Hello from FastAPI 🚀"}
+
+class Features(BaseModel):
+    features: list[float]
+
+@app.post("/predict")
+def predict(data: Features):
+    X = np.array(data.features).reshape(1, -1)
+    prediction = model.predict(X)
+    return {"prediction": int(prediction[0])}
