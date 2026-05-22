8f641728c88c12c5b6200818ba9a929a3a57d9e0
ismail01-jl <jellitiismail2002@gmail.com>
2026-05-22 14:00:20 +0100

    Cors file Fixing Origins

--- app/middleware/cors.py
+++ app/middleware/cors.py
@@ -3,7 +3,10 @@ from fastapi.middleware.cors import CORSMiddleware
 def setup_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
+        allow_origins=[
+            "http://localhost:5173",
+            "https://ecosmart-frontend.onrender.com"
+        ],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
